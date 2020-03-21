dd8784579d3fdc5c417001d9a4cc78003d370259
MoonJumpMania <moonjumpmania@gmail.com>
2020-03-21 13:09:27 -0400

    Add Source Code
    Added the source code to the "Source Code" directory.

---
+++ Source code/Snake/Assets/SpawnFood.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour {
+    
+    //food prefab
+    public GameObject foodPrefab;
+
+    //borders
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    //spawns one piece of food
+    void Start() {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+   
+    //spawns the food within
+    void Spawn() {
+        //random x position for food between the left and right borders
+        int x = (int)Random.Range(borderLeft.position.x,
+                                  borderRight.position.x);
+
+        //random y position for food between the top and bottom borders
+        int y = (int)Random.Range(borderTop.position.y, 
+                                  borderBottom.position.y);
+
+        //instantiating the positioning
+        Instantiate(foodPrefab, 
+                    new Vector2(x, y), 
+                    Quaternion.identity);
+
+    }
+
+    
+}
