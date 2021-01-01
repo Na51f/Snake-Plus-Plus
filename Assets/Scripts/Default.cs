1d805eefdda7c129b3e19fb63398313e551315ed
MoonJumpMania <moonjumpmania@gmail.com>
2021-01-01 16:31:33 -0500

    Control system made

---
+++ Assets/Scripts/Default.cs
@@ -0,0 +1,10 @@
+﻿namespace Scripts
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
---
+++ Assets/Scripts/Events/Setup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Scripts.Snakes;
+using UnityEngine;
+
+namespace Scripts.Events
+{
+    [ExecuteInEditMode]
+    public class Setup : MonoBehaviour
+    {
+        [SerializeField] private List<Snake> snakeList;
+        [SerializeField] private GameObject snakePrefab;
+
+        public void Awake()
+        {
+            snakeList = new List<Snake>();
+        }
+    }
+}
